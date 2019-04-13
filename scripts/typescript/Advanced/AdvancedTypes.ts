73db957f0746d6f0506b25f7e3cc849b9124fcd5
Allen Conway <allenc@magenic.com>
2019-04-13 00:32:56 -0400

    Adding AdvancedTypes samples

--- scripts/typescript/Advanced/AdvancedTypes.ts
+++ scripts/typescript/Advanced/AdvancedTypes.ts
@@ -13,10 +13,11 @@ namespace IntersectionTypes {
     }
 
     type Admin = Employee & User;
-    let admin: Admin;
-    // let myAdmin = new admin;
+    let admin: Admin;    
+    // console.log(admin.getHours());
+    // console.log(admin.getLastLogin());
 
-
+    // Utility method that uses '&' intersection type combining multiple types into one
     function extend<First, Second>(first: First, second: Second): First & Second {
         const result: Partial<First & Second> = {};
         for (const prop in first) {
@@ -31,30 +32,135 @@ namespace IntersectionTypes {
         }
         return <First & Second>result;
     }
-    
-    class Person {
-        constructor(public name: string) { }
 
-        test() {
+    const adminUser = extend(Employee.prototype, User.prototype);    
+    console.log(adminUser.getHours());
+    console.log(adminUser.getLastLogin());
 
-        }
+}
+
+namespace UnionTypes {
+
+    function allowUnionTypeParameter(input: string|number|null) {
+        return input;
     }
-    
-    interface Loggable {
-        log(name: string): void;
+
+    console.log(allowUnionTypeParameter(123));
+    console.log(allowUnionTypeParameter("Hello"));
+    console.log(allowUnionTypeParameter(null));
+    // Lines below are not allowed    
+    // console.log(allowUnionTypeParameter(true));
+    // console.log(allowUnionTypeParameter(undefined));
+
+    export interface Engine {
+        manufacturer: string;
+        size: number;
+        horsePower: number;
+        showEngineMeta();
     }
-    
-    class ConsoleLogger implements Loggable {
-        log(name) {
-            console.log(`Hello, I'm ${name}.`);
-        }
 
-        test1() {
-
-        }
+    export interface Car {
+        color: string;
+        model: string;
+        manufacturer: string;
+        showCarMeta();
     }
-    
-    const jim = extend(new Person('Jim'), ConsoleLogger.prototype);
-    jim.log(jim.name);
 
+    export function getAutoInformation():Engine | Car {
+        // ...logic to determine what's being returned
+        let myCar: Car = {
+            color: "Red",
+            model: "Mustang",
+            manufacturer: "Ford",
+            showCarMeta: () => {return 'Hello from myCar';}
+        }
+        return myCar;
+    }
+
+    let carInformation = getAutoInformation();
+    console.log(carInformation.manufacturer);
+    // The line below is not valid as using a Union type, we can only see memebers that are in common (i.e. 'manufacturer')
+    // console.log(carInformation.model);
+}
+
+namespace StringLiteralTypes {
+
+    type stringLiteralColors = "red" | "black" | "green" | "orange";
+
+    // Notice use of Union types in both input and return values being used :)
+    function showColor(color: stringLiteralColors | Colors): string | number {
+        return color;
+    }
+
+    // String literals will have intellisense on the string literal values
+    console.log(showColor("red"));
+    console.log(showColor("black"));
+
+    enum Colors {
+        Red = 1,
+        Black = 2,
+        Green = 3,
+        Orange = 4
+    }
+
+    // Enums will have intellisense on the enum options
+    console.log(showColor(Colors.Black));
+    console.log(showColor(Colors.Green));
+
+    // Sample using a numeric literal type
+    function gallonsOfGasEligableDiscount(gallons: 5 | 10 | 15 | 20): number {
+        return gallons;
+    }
+
+    console.log(gallonsOfGasEligableDiscount(10));
+    // The line below is not a valid numeric literal
+    // console.log(gallonsOfGasEligableDiscount(12));
+
+}
+
+namespace TypeGuards {
+
+    // Brut force method checking with type assertions littered everywhere
+    let myAuto = UnionTypes.getAutoInformation()
+    if((<UnionTypes.Car>myAuto).showCarMeta) {
+        console.log((<UnionTypes.Car>myAuto).showCarMeta());
+    }
+    else{
+        console.log((<UnionTypes.Engine>myAuto).showEngineMeta());
+    }
+
+    // Instead we can use a custom defined Type Guard functions
+    function isCar(autoMeta: UnionTypes.Engine | UnionTypes.Car): autoMeta is UnionTypes.Car {
+        return (<UnionTypes.Car>myAuto).showCarMeta !== undefined;
+    }
+
+    function isEngine(autoMeta: UnionTypes.Engine | UnionTypes.Car): autoMeta is UnionTypes.Engine {
+        return (<UnionTypes.Engine>myAuto).showEngineMeta !== undefined;
+    }
+
+    if(isCar(myAuto)) {
+        console.log(myAuto.showCarMeta());
+    }
+    else {
+        // Notice with intellisense the compiler knows that in this else branch we are not a Car and so must be an engine
+        myAuto.showEngineMeta();
+    }
+}
+
+namespace NullableTypes {
+
+    // For this demo, make sure that in tsconfig.json --strictNullChecks
+    let firstName: string = "Allen";
+    // Toggle --strictNullChecks and watch behavior
+    firstName = null;
+    //This is acceptable when included explicitly as a union type
+    let lastName: string | null = "Conway";
+    lastName = null;
+
+    // Test removing undefined while strictNullChecks is set to true
+    let phoneNumber: string | number | null | undefined;
+    phoneNumber = "4071234567";
+    phoneNumber = 4071234567;
+    phoneNumber = null;
+    phoneNumber = undefined;
 }
